5c02cc26153a1c7b695c61be4891c10bbb521740
Mealea Hunter <54214493+MealeaHunter@users.noreply.github.com>
2021-07-10 18:08:05 -0400

    Create mock data service

---
+++ src/app/mock-data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MockDataService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  get() {
+    return this.httpClient.get('https://randomuser.me/api/?results=12')
+  }
+}
