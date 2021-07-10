0f0fe9cfbc8ee14953610f4b188e0444fa603ebd
Mealea Hunter <54214493+MealeaHunter@users.noreply.github.com>
2021-07-10 18:22:38 -0400

    Get mock data for user to build web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from './app.component';
-
+import { UserCardComponent } from './user-card/user-card.component';
+import { createCustomElement } from '@angular/elements';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, UserCardComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(UserCardComponent, { injector });
+    customElements.define('user-card', el);
+  }
+
+  ngDoBootstrap() { }
+}
---
+++ src/app/user-card/user-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MockDataService } from '../mock-data.service';
+
+@Component({
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.css']
+})
+export class UserCardComponent implements OnInit {
+  users: any;
+
+  constructor(private mockDataService: MockDataService) { }
+
+  ngOnInit(): void {
+    this.mockDataService.get().subscribe((data: any) => {
+      console.log(data)
+
+      this.users = data['results'];
+    });
+  }
+
+}
